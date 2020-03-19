2b23f2f9aa7645b0a28e7269ac44375ece64adf2
khen0002 <mehdi.khenissi@etudiant.univ-reims.fr>
2020-03-19 20:07:24 +0100

    test Chess

--- test/chess/ChessboardTest.java
+++ test/chess/ChessboardTest.java
@@ -4,11 +4,103 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
+import chess.pieces.Piece;
+import chess.pieces.Rook;
+import chess.util.Color;
+import chess.util.Position;
+
 class ChessboardTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getPieceTest() {
+		Chessboard b = new Chessboard();
+		Piece p =  new Rook(this, new Position(7, 0), Color.BLACK);
+		assertEquals(p,b.getPiece(7, 0));
+	}
+	@Test
+	void getPiecePosTest() {
+		Chessboard b = new Chessboard();
+		Piece p =  new Rook(this, new Position(7, 0), Color.BLACK);
+		assertEquals(p,b.getPiece(new Position(7, 0)));
+	}
+	@Test 
+	void addHistoricTest() {
+		Chessboard b = new Chessboard();
+		Position Old = new Position(0,1);
+		Position New = new Position(0,2);
+		b.addHistoric(" ROOK",Old,New);
+		String s = "1 Déplacement du ROOK de la case "+Old.toAlgebraicNotation() + " à la case " New.toAlgebraicNotation()+".";
+		assertEquals(s,b.printHistoric());
+	}
+	@Test
+	void printHistoricTest() {
+		Chessboard b = new Chessboard();
+		Position Old = new Position(0,1);
+		Position New = new Position(0,2);
+		b.addHistoric(" ROOK",Old,New);
+		String s = "1 Déplacement du ROOK de la case "+Old.toAlgebraicNotation() + " à la case " New.toAlgebraicNotation()+".";
+		assertEquals(s,b.printHistoric());
+	}
+	@Test 
+	void isPiecePresentOnSameColumnBetweenFalseTest() {
+		Chessboard b = new Chessboard();
+		Position one = new Position(0,1);
+		Position two = new Position(0,2);
+		assertEquals(false,b.isPiecePresentOnSameColumnBetween(one,two));
+		
+		
+	}
+	@Test 
+	void isPiecePresentOnSameColumnBetweenTest() {
+		Chessboard b = new Chessboard();
+		Position one = new Position(0,1);
+		Position two = new Position(0,3);
+		Piece p =  new Rook(this, new Position(0, 1), Color.BLACK);
+		Position pos = new Position(0,2);
+		b.setPiece(pos, p);
+		assertEquals(true,b.isPiecePresentOnSameColumnBetween(one,two));
+		
+		
+	}
+	@Test 
+	void isPiecePresentOnSameDiagonalBetweenFalseTest() {
+		Chessboard b = new Chessboard();
+		Position one = new Position(1,1);
+		Position two = new Position(2,2);
+		assertEquals(false,b.isPiecePresentOnSameDiagonalBetween(one,two));	
+	}
+	@Test 
+	void isPiecePresentOnSameDiagonalBetweenTest() {
+		Chessboard b = new Chessboard();
+		Position one = new Position(1,1);
+		Position two = new Position(3,3);
+		Piece p =  new Rook(this, new Position(2, 2), Color.BLACK);
+		Position pos = new Position(2,2);
+		assertEquals(true,b.isPiecePresentOnSameDiagonalBetween(one,two));	
+	}
+	@Test 
+	void isPiecePresentOnSameLineBetweenFalseTest() {
+		Chessboard b = new Chessboard();
+		Position one = new Position(1,0);
+		Position two = new Position(2,0);
+		assertEquals(false,b.isPiecePresentOnSameDiagonalBetween(one,two));	
+	}
+	@Test 
+	void isPiecePresentOnSameLineBetweenTest() {
+		Chessboard b = new Chessboard();
+		Position one = new Position(1,0);
+		Position two = new Position(3,0);
+		Piece p =  new Rook(this, new Position(2, 0), Color.BLACK);
+		Position pos = new Position(2,0);
+		assertEquals(true,b.isPiecePresentOnSameDiagonalBetween(one,two));	
+	}
+	@Test
+	void setPiece() {
+		Chessboard b = new Chessboard();
+		Piece p =  new Rook(this, new Position(0, 1), Color.BLACK);
+		Position pos = new Position(2,2);
+		b.setPiece(pos, p);
+		assertEquals(p.getName(),b.getPiece(pos).getName());		
 	}
 
 }
--- test/chess/GameTest.java
+++ test/chess/GameTest.java
@@ -4,11 +4,56 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
+import chess.util.Color;
+
 class GameTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void TurnTest() {
+		Game g = new Game("Mehdi","Charles");
+		g.turn(new Position(0,0),new Position(0,3));
+		assertEquals(Color.BLACK,g.getCurrentColor())
 	}
+
+	@Test
+	void NullStartTurnTest() {
+		Game g = new Game("Mehdi","Charles");
+		try {
+			g.turn(null,new Position(0,7));
+			fail("Fail");
+		} catch(ChessMoveException e) {}
+		
+	}
+	@Test
+	void PositionIsTakenTurnTest() {
+		Game g = new Game("Mehdi","Charles");
+		try {
+			g.turn(new Position(0,0),new Position(0,7));
+			fail("Fail");
+		} catch(ChessMoveException e) {}
+		
+	}
+	@Test
+	void getBlackPlayerNameTest() {
+		Game g = new Game("Mehdi","Charles");
+		assertEquals("Mehdi",g.getBlackPlayerName());
+	}
+	@Test
+	void getWhitePlayerNameTest() {
+		Game g = new Game("Mehdi","Charles");
+		assertEquals("Charles",g.getWhitePlayerName());
+	}
+	@Test
+	void getCurrentColorTest() {
+		Game g = new Game("Mehdi","Charles");
+		assertEquals(Color.WHITE,g.getCurrentColor());
+	}
+	@Test
+	void getCurrentColorAfterTurnTest() {
+		Game g = new Game("Mehdi","Charles");
+		g.turn(new Position(0,0),new Position(0,3));
+		assertEquals(Color.BLACK,g.getCurrentColor());
+	}
+
 
 }
